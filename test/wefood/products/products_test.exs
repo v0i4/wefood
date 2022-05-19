da81afa6dc5c001698c7208ef8f731a043022b88
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-05-19 15:51:07 -0300

    improving tests for products create

--- test/wefood/products/products_test.exs
+++ test/wefood/products/products_test.exs
@@ -16,4 +16,13 @@ defmodule Wefood.ProductsTest do
     assert product.price == payload.price
     assert product.name == payload.name
   end
+
+  test "given a product with the same name, should throw an error message" do
+    payload = %{name: "pizza", size: "XL", price: 100, description: "margherita"}
+    assert {:ok, %Product{} = product} = Products.create_product(payload)
+    assert {:error, changeset} = Products.create_product(payload)
+    assert "has already been taken" in errors_on(changeset).name
+    assert %{name: ["has already been taken"]} = errors_on(changeset)
+
+  end
 end
