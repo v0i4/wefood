2a83572154fe83431d7656f51dc4f430f5b39a7a
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-05-23 16:27:25 -0300

    insert_modal_create_product

--- lib/wefood/products/product.ex
+++ lib/wefood/products/product.ex
@@ -10,7 +10,7 @@ defmodule Wefood.Products.Product do
   @foreign_key_tipe :binary_id
   schema "products" do
     field :name, :string
-    field :price, :integer
+    field :price, Money.Ecto.Amount.Type
     field :size, :string
     field :description, :string
 
--- lib/wefood_web/live/admin/products/form/form.ex
+++ lib/wefood_web/live/admin/products/form/form.ex
@@ -6,20 +6,33 @@ defmodule WefoodWeb.Admin.Products.Form do
 
   def update(assigns, socket) do
     changeset = Products.change_product()
+
     {:ok,
-    socket
-    |> assign(assigns)
-    |> assign(changeset: changeset)
-    |> assign(product: %Product{})
-  }
+     socket
+     |> assign(assigns)
+     |> assign(changeset: changeset)
+     |> assign(product: %Product{})}
   end
 
   def handle_event("validate", %{"product" => product_params}, socket) do
-    changeset = socket.assigns.product
-    |> Products.change_product(product_params)
-    |> Map.put(:action, :validate)
+    changeset =
+      socket.assigns.product
+      |> Products.change_product(product_params)
+      |> Map.put(:action, :validate)
 
     {:noreply, assign(socket, :changeset, changeset)}
+  end
 
+  def handle_event("save", %{"product" => product_params}, socket) do
+    case Products.create_product(product_params) do
+      {:ok, _product} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Product has created..")
+         |> push_redirect(to: "/admin/products")}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
