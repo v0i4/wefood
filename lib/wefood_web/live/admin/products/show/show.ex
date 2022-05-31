cb9197acbe455fe1ad638dca0f3a03e8922f52ca
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-05-31 20:37:29 -0300

    show products done

---
+++ lib/wefood_web/live/admin/products/show/show.ex
@@ -0,0 +1,15 @@
+defmodule WefoodWeb.Admin.ProductLive.Show do
+  @moduledoc false
+  use WefoodWeb, :live_view
+  alias Wefood.Products
+
+  def mount(_p, _s, socket), do: {:ok, socket}
+
+  def handle_params(%{"id" => id}, _url, socket) do
+    product = Products.get!(id)
+    {:noreply,
+    socket
+    |> assign(product: product)
+    |> assign(page_title: "show")}
+  end
+end
