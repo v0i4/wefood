5713b9d71bf263e3b516eb3d4c7f37954d2357e0
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-06-12 19:56:19 -0300

    waffle + phoenix liveview - upload files

---
+++ lib/wefood/products/product_image.ex
@@ -0,0 +1,20 @@
+defmodule Wefood.Products.ProductImage do
+  use Waffle.Definition
+  use Waffle.Ecto.Definition
+
+  @versions [:original, :thumb]
+  @extension_whitelist ~w(.png .jpeg .jpg)
+
+  def validate({file, _}) do
+    file_extension = file.file_name |> Path.extname |> String.downcase
+
+    case Enum.member?(@extension_whitelist, file_extension) do
+      true -> :ok
+      false -> {:error, "file type is invalid"}
+    end
+  end
+
+  def storage_dir(_, {file, product}) do
+    "priv/static/uploads/products/#{product.name}"
+  end
+end
