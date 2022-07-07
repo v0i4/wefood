dc2a0ed99775738f5e643afe1c0708c6f106bb44
antonio vasconcellos <73661148+v0i4@users.noreply.github.com>
2022-07-07 00:40:15 -0300

    Carts tested

---
+++ lib/wefood/carts/boundary/cart_session.ex
@@ -0,0 +1,63 @@
+defmodule Wefood.Carts.Boundary.CartSession do
+  @moduledoc """
+  Cart Cache server handler
+  """
+  use GenServer
+  alias Wefood.Carts.Core.HandleCarts, as: Cart
+
+  def start_link(_), do: GenServer.start_link(__MODULE__, :cart_session, name: :cart_session)
+
+  def init(name) do
+    :ets.new(name, [:set, :public, :named_table])
+    {:ok, name}
+  end
+
+  def handle_cast({:create, cart_id}, name) do
+    case find_cart(name, cart_id) do
+      {:not_found, []} -> :ets.insert(name, {cart_id, Cart.create_carts(cart_id)})
+      {:ok, _value} -> :return_cache
+    end
+
+    {:noreply, name}
+  end
+
+  def handle_cast({:add, cart_id, product}, name) do
+    {:ok, cart} = find_cart(name, cart_id)
+    cart = Cart.add(cart, product)
+    :ets.insert(name, {cart_id, cart})
+    {:noreply, name}
+  end
+
+  def handle_call({:inc, cart_id, product}, _from, name) do
+    {:ok, cart} = find_cart(name, cart_id)
+    cart = Cart.inc(cart, product)
+    :ets.insert(name, {cart_id, cart})
+    {:reply, cart, name}
+  end
+
+  def handle_call({:dec, cart_id, product}, _from, name) do
+    {:ok, cart} = find_cart(name, cart_id)
+    cart = Cart.dec(cart, product)
+    :ets.insert(name, {cart_id, cart})
+    {:reply, cart, name}
+  end
+
+  def handle_call({:remove, cart_id, product}, _from, name) do
+    {:ok, cart} = find_cart(name, cart_id)
+    cart = Cart.remove(cart, product)
+    :ets.insert(name, {cart_id, cart})
+    {:reply, cart, name}
+  end
+
+  def handle_call({:get, cart_id}, _from, name) do
+    {:ok, cart} = find_cart(name, cart_id)
+    {:reply, cart, name}
+  end
+
+  defp find_cart(name, cart_id) do
+    case :ets.lookup(name, cart_id) do
+      [] -> {:not_found, []}
+      [{_cart_id, value}] -> {:ok, value}
+    end
+  end
+end
--- test/wefood/carts/carts_test.exs
+++ test/wefood/carts/carts_test.exs
@@ -1,8 +1,52 @@
 defmodule Wefood.CartsTest do
   use Wefood.DataCase
   alias Wefood.Carts
+  import Wefood.Factory
 
   test "should create session" do
-    assert :ok == Carts.create_session(444)
+    assert :ok == Carts.create(444)
+  end
+
+  test "should create session twice" do
+    assert :ok == Carts.create(444)
+    assert :ok == Carts.create(444)
+  end
+
+  test "should create a new product" do
+    cart_id = Ecto.UUID.generate()
+    assert :ok == Carts.create(cart_id)
+    product = insert(:product)
+
+    assert :ok == Carts.add(cart_id, product)
+    assert 1 == Carts.get(cart_id).total_qty
+  end
+
+  test "should add the same item" do
+    cart_id = Ecto.UUID.generate()
+    assert :ok == Carts.create(cart_id)
+    product = insert(:product)
+
+    assert :ok == Carts.add(cart_id, product)
+    assert 2 == Carts.inc(cart_id, product.id).total_qty
+  end
+
+  test "should dec the same item" do
+    cart_id = Ecto.UUID.generate()
+    assert :ok == Carts.create(cart_id)
+    product = insert(:product)
+
+    assert :ok == Carts.add(cart_id, product)
+    assert 0 == Carts.dec(cart_id, product.id).total_qty
+  end
+
+  test "should remove product" do
+    cart_id = Ecto.UUID.generate()
+    assert :ok == Carts.create(cart_id)
+    product = insert(:product)
+
+    assert :ok == Carts.add(cart_id, product)
+    assert :ok == Carts.add(cart_id, product)
+    assert 2 == Carts.get(cart_id).total_qty
+    assert 0 == Carts.remove(cart_id, product.id).total_qty
   end
 end
